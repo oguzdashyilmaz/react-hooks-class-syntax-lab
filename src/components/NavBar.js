a01d0587bdfce54b1afac5add9fde6afb8901ab6
Oguz <yilmazoguz@outlook.com>
2022-03-10 16:44:16 +0300

    Create NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-    </nav>
-  );
+
+class Navbar extends React.Component{
+  render() {
+    return (
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+      </nav>
+    )
+  }
 }
 
 export default NavBar;
